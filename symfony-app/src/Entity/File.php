ee24b1b61d8555ab07056253e0cb0b402ffd219e
elitecolors <saidaniahmed125@gmail.com>
2024-03-21 15:38:11 +0100

    test database

---
+++ symfony-app/src/Entity/File.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FileRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FileRepository::class)]
+class File
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $originalName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $filePath = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $fullPath = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getOriginalName(): ?string
+    {
+        return $this->originalName;
+    }
+
+    public function setOriginalName(string $originalName): static
+    {
+        $this->originalName = $originalName;
+
+        return $this;
+    }
+
+    public function getFilePath(): ?string
+    {
+        return $this->filePath;
+    }
+
+    public function setFilePath(string $filePath): static
+    {
+        $this->filePath = $filePath;
+
+        return $this;
+    }
+
+    public function getFullPath(): ?string
+    {
+        return $this->fullPath;
+    }
+
+    public function setFullPath(string $fullPath): static
+    {
+        $this->fullPath = $fullPath;
+
+        return $this;
+    }
+}
