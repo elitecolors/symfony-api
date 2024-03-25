f26dd701f758c67d2427a134a7d569567e240465
elitecolors <saidaniahmed125@gmail.com>
2024-03-25 11:30:18 +0100

    add nelmio api doc

--- symfony-app/config/bundles.php
+++ symfony-app/config/bundles.php
@@ -6,4 +6,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
