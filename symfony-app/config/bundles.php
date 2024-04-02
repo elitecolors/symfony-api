b597c75118cff7f45dcfbcc6fee90df2bec3359e
elitecolors <saidaniahmed125@gmail.com>
2024-04-02 12:08:32 +0200

    add dama_doctrine_test_bundle

--- symfony-app/config/bundles.php
+++ symfony-app/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
